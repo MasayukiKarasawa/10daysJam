c6a8445c5a0665123399c319b8442fa344eab40c
雅之 柄澤 <k022g0051@m.neec.ac.jp>
2023-09-05 16:19:24 +0900

    敵ランダム生成、自機移動、色変更まで

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <Novice.h>
-
+#include<time.h>
+#include<stdlib.h>
 const char kWindowTitle[] = "切り替えの達人";
 enum number {
 	TITLE,
@@ -12,17 +13,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	// ライブラリの初期化
 	Novice::Initialize(kWindowTitle, 1000, 720);
+	
 
 	int number = 0;
 	int playerX = 500;
 	int playerY = 600;
 	int radius = 64;
-	//int color[10] = {};
+	int enemyX[10] = {};
+	int enemyY[10] = {};
 	int MyColor = 0;
 	int Speed = 10;
-
-
-
+	int enemySpeed = 5;
+	int enemyFlag[10] = {};
+	int color[3] = {0,1,2};
 
 	// キー入力結果を受け取る箱
 	char keys[256] = {0};
@@ -53,16 +56,56 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			}
 			break;
 		case GAME:
-			if (keys[DIK_A] ) {
+			//移動
+			if (keys[DIK_A] || keys[DIK_LEFT]) {
+				playerX -= Speed;
+
+			}
+			if (keys[DIK_D] || keys[DIK_RIGHT]) {
+				playerX += Speed;
+			}
+			if (playerX + radius>= 1000) {
+				playerX = 1000-radius;
+			}
+			if (playerX - radius<= 0) {
+				playerX = 0+radius;
+			}
+			//色変更
+			if (keys[DIK_J] ) {
 				MyColor = 0;
 			}
-			if (keys[DIK_S] ) {
+			if (keys[DIK_K] ) {
 				MyColor = 1;
 			}
-			if (keys[DIK_D] ) {
+			if (keys[DIK_L] ) {
 				MyColor = 2;
 			}
+			//敵乱数
+			if (rand() % 100 == 0) {
+				for (int i = 0; i < 5; i++) {
+					if (enemyFlag[i] == 0) {
+						enemyFlag[i] = 1;
+						enemyX[i]=  rand() % 1000;
+						enemyY[i] = rand() % 10;
+						color[i] = rand() % 3;
+						break;
 
+					}
+					
+				}
+			}
+			//敵移動
+				for (int i = 0; i < 10; i++) {
+					if (enemyFlag[i] == 1) {
+                   enemyY[i] += enemySpeed;
+					}
+					
+					if (enemyY[i] >= 720) {
+						enemyFlag[i] = 0;
+						enemyY[i] = 0;
+				     }
+				}
+			
 
 			break;
 		case SCORE:
@@ -74,6 +117,9 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↑更新処理ここまで
 		///
+		/// 	
+		/// ↓描画処理ここから
+		///
 		switch (number)
 		{
 		case TITLE:
@@ -84,22 +130,45 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 			break;
 		case GAME:
 			Novice::ScreenPrintf(0, 0, "Game");
-		
-			//自機
-			Novice::DrawEllipse(playerX, playerY, radius, radius, 0.0f, WHITE, kFillModeSolid);
-
-			//敵
-			Novice::DrawEllipse(500, -100, radius, radius, 0.0f, WHITE, kFillModeSolid);
-
+			if (MyColor == 0) {
+				//自機
+				Novice::DrawEllipse(playerX, playerY, radius, radius, 0.0f, RED, kFillModeSolid);
+		    }
+			if (MyColor == 1) {
+				//自機
+				Novice::DrawEllipse(playerX, playerY, radius, radius, 0.0f, BLUE, kFillModeSolid);
+			}
+			if (MyColor == 2) {
+				//自機
+				Novice::DrawEllipse(playerX, playerY, radius, radius, 0.0f, GREEN, kFillModeSolid);
+			}
+			
+				for (int i = 0; i < 10; i++) {
+					if (enemyFlag[i] == 1) {
+						if (color[i] == 0) {
+						    //敵
+							Novice::DrawEllipse(enemyX[i], enemyY[i], radius, radius, 0.0f, RED, kFillModeSolid);
+						}
+						if (color[i] == 1) {
+							//敵
+							Novice::DrawEllipse(enemyX[i], enemyY[i], radius, radius, 0.0f, BLUE, kFillModeSolid);
+						}
+						if (color[i] == 2) {
+							//敵
+							Novice::DrawEllipse(enemyX[i], enemyY[i], radius, radius, 0.0f, GREEN, kFillModeSolid);
+						}
+					}
+				
+				}
+			
+			
 
 			break;
 		case SCORE:
 			Novice::ScreenPrintf(0, 0, "Score");
 			break;
 		}
-		///
-		/// ↓描画処理ここから
-		///
+	
 	
 		///
 		/// ↑描画処理ここまで
